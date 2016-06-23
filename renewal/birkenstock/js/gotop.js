5ee02b989ef65b2710840ada2a877b8d59ba35b2
miraep <mirae6615@gmail.com>
2016-06-23 10:51:21 +0900

    삭제

--- renewal/birkenstock/js/gotop.js
+++
@@ -1,7 +0,0 @@
-//top
-
-function gotop(){
-	$('#footerBox').find('button').on('click', function(){
-		$('html,body').animate({'scrollTop':0},700);
-	});
-};
--- renewal/birkenstock/js/topadd.js
+++
@@ -1,6 +0,0 @@
-// 팝업창
-function topAdd(ta){
-	ta.find('button').on('click',function(){
-		ta.slideUp();
-	});
-};
